44d516cd4eb931c999b95d64413eb0da1139ad15
s33yuh <yunkab0b@gmail.com>
2024-03-05 15:39:34 +1300

    Added meme inspo and empty meme template to repo
    Modified meme.R

--- Project1/meme.R
+++ Project1/meme.R
@@ -1,1 +1,8 @@
 library(magick)
+# link to meme: https://knowyourmeme.com/memes/in-the-studio-like-drop-it/photos/page/2
+
+drop_it_meme <- "drop_it_meme_template.png" %>%
+  image_read() %>%
+  image_crop("500x500")
+
+drop_it_meme
