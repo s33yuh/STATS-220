dfd10928d435975d1306e3bbc18f9411231f2cfb
s33yuh <yunkab0b@gmail.com>
2024-03-13 17:06:37 +1300

    Finished Kodak meme and wrote Kodak meme and Drop it meme to image file

--- Project1/meme.R
+++ Project1/meme.R
@@ -39,21 +39,74 @@ drop_it_meme <- "drop_it_meme_template.png" %>%
   
   
   # "deep fry" meme by sharpening using convolution
-  image_convolve('DoG:0,0,2', scaling = '100, 20%') 
+  image_convolve('DoG:0,0,2', scaling = '100, 20%') %>%
+  
+  # write finished meme to image file
+  image_write("my_drop_it_meme.jpg")
 
   #%>%
   #image_convert(type = "Bilevel")
   
 
-drop_it_meme
+#drop_it_meme
 
 
-kodak_meme_text <- image_blank(500, 500, "gray") %>%
-                  image_annotate("yur", gravity = "north", 
-                                 weight = 700, size = 40) %>%
-                  image_mosaic("kodak.jpg")
+# Kodak Black Meme:
+
+
+# Kodak Black's face 
+kodak_face <- "kodak.jpg" %>%
+              image_read() %>%
+              # scale and crop image
+              image_crop("400x300") %>%
+              image_scale("286") %>%
+              # modulate image to add character
+              image_modulate(saturation = 130, brightness = 110, hue = 110) %>%
+              # add thin yellow border
+              image_border("yellow", "1x1")
+              
+# person falling out of car (slightly different from inspo)
+thrown_out <- "falling_out_of_car.jpg" %>%
+              image_read() %>%
+              # crop and scale image
+              image_crop("600x350+50") %>%
+              image_scale("286") %>%
+              # modulate image to add character
+              image_modulate(saturation = 150, brightness = 110, hue = 110) %>%
+              # add thin yellow border
+              image_border("yellow", "1x1")
                   
-kodak_meme_text
+
+
+
+# meme background boxes
+kodak_box <- image_blank(290, 290, "black") %>%
+             image_composite(kodak_face, offset = "+1+40")
+thrown_out_box <- image_blank(290, 290, "black") %>%
+                  image_composite(thrown_out, offset = "+1+60")
+
+
+
+# meme text box
+text_box <- image_blank(500, 350, "gray") %>%
+  image_annotate("\"Can we listen to some J. cole\"", 
+                 gravity = "north", location = "+0+15",
+                 weight = 500, size = 30, font = "sans")
+
+# append both images
+kodak_meme_body <- c(kodak_box, thrown_out_box) %>%
+                   image_append() %>%
+                   image_scale("490")
+
+# combine text box and image box
+kodak_meme <- text_box %>% 
+              image_composite(kodak_meme_body, offset = "+5+70") %>%
+              # write finished meme to .jpg file
+              image_write("my_kodak_meme.jpg")
+              
+  
+
+kodak_meme
 
 # animation
 frames <- c(drop_it_meme)
