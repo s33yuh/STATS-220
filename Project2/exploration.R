722e2ce153e38c79fefbf7c68661b3f3f541783d
Boris Kishi-Rychkov <bkis790@UoA.auckland.ac.nz>
2024-03-27 22:20:20 +1300

    Worked on R script

--- Project2/exploration.R
+++ Project2/exploration.R
@@ -1,2 +1,5 @@
 library(tidyverse)
-# published link https://docs.google.com/spreadsheets/d/e/2PACX-1vSn9mbPcQDLTzHf8J8DtdDz6RxgBMGvTpcYa4rJRYm1UldkV8edd5_rrGEbuBOu9r0Jn68r5pPsTkE4/pub?output=csv
+# published link 
+published_data_link <- "https://docs.google.com/spreadsheets/d/e/2PACX-1vSn9mbPcQDLTzHf8J8DtdDz6RxgBMGvTpcYa4rJRYm1UldkV8edd5_rrGEbuBOu9r0Jn68r5pPsTkE4/pub?output=csv"
+
+data <- read_csv(published_data_link)
